95bc9984674375aabba72c7c2f7b7f0c5498d4c4
igsci <igsciquotes@gmail.com>
2024-05-14 22:13:24 +0530

    test

---
+++ test1.py
@@ -0,0 +1,12 @@
+```python
+def main():
+  """
+  This function emulates the behavior of the COBOL "HELLO" program.
+  It prints "HELLOWORLD" to the console, simulating the DISPLAY statement.
+  """
+  print("HELLOWORLD")
+
+# Simulating the implicit 'STOP RUN' in COBOL
+if __name__ == "__main__":
+  main()
+```
